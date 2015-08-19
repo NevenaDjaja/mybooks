868d1734d81d5562f69ae1a91eecb99cea78c155
Nevena Djaja <nevena.djaja@gmail.com>
2015-08-18 21:13:16 -0700

    added migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815214402) do
+ActiveRecord::Schema.define(version: 20150819033800) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150815214402) do
     t.text     "notes"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.float    "rating"
   end
 
 end
