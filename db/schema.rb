60aeed88408d70766da6be6a8e7eca3b02e9aabb
Nevena Djaja <nevena.djaja@gmail.com>
2015-08-21 16:13:43 -0700

    hardcoded the view for now, added migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819033800) do
+ActiveRecord::Schema.define(version: 20150819042739) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20150819033800) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.float    "rating"
+    t.string   "slug"
   end
 
 end
